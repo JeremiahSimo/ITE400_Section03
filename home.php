58dd2496a0ee8a2f309ede12598b4069c42d1c45
saintSchwarze <jamesoliveremano12@gmail.com>
2024-12-04 08:35:58 +0800

    wahaha

--- home.php
+++ home.php
@@ -85,6 +85,7 @@ footer {
     <li><a href="home.php?page=london" class="<?php echo ($_GET['page'] ?? '') === 'london'  ? 'active' : ''; ?>">London</a></li>
       <li><a href="home.php?page=paris" class="<?php echo ($_GET['page'] ?? '') === 'paris'  ? 'active' : ''; ?>">Paris</a></li>
       <li><a href="home.php?page=tokyo" class="<?php echo ($_GET['page'] ?? '') === 'tokyo'  ? 'active' : ''; ?>">Tokyo</a></li>
+      <li><a href="home.php?page=berlin" class="<?php echo ($_GET['page'] ?? '') === 'berlin'  ? 'active' : ''; ?>">Berlin</a></li>
     </ul>
   </nav>
   
