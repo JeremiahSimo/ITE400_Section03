75bf05d82993a295e7855d257872e221812407ca
unknown <loui.zapico.coc@phinmaed.com>
2024-12-11 08:16:52 +0800

    my personal info

--- home.php
+++ home.php
@@ -84,6 +84,7 @@ footer {
     <li><a href="home.php?page=london" class="<?php echo ($_GET['page'] ?? '') === 'london'  ? 'active' : ''; ?>">London</a></li>
       <li><a href="home.php?page=paris" class="<?php echo ($_GET['page'] ?? '') === 'paris'  ? 'active' : ''; ?>">Paris</a></li>
       <li><a href="home.php?page=tokyo" class="<?php echo ($_GET['page'] ?? '') === 'tokyo'  ? 'active' : ''; ?>">Tokyo</a></li>
+      <li><a href="home.php?page=zapico" class="<?php echo ($_GET['page'] ?? '') === 'zapico'  ? 'active' : ''; ?>">Zapico</a></li>
     </ul>
   </nav>
   
@@ -103,6 +104,9 @@ footer {
                         case 'tokyo':
                             include 'tokyo.php';
                             break;
+                            case 'zapico':
+                              include 'zapico.php';
+                              break;
             
         }
     }
---
+++ zapico.php
@@ -0,0 +1,6 @@
+
+<h1>Louie Cassandra Zapico</h1>
+    <p>CITE Student</p>
+    <p>Bulua, Zone 2 Lower, CDO City</p>
+    <p>Gender: Female</p>
+  
