ca4ad3e5d67af5f07580ac2d7a1e10e7be81c5d1
Trixy Genon <trca.genon.coc@phinmaed.com>
2024-12-11 17:32:42 +0800

    created by genon

--- home.php
+++ home.php
@@ -71,7 +71,7 @@ footer {
 </head>
 <body>
 
-<h2>Abduwlahab, mohammadjarer A.</h2>
+<h2>Genon, Trixy C.</h2>
 <p>this is our website</p>
 
 <header>
@@ -84,6 +84,7 @@ footer {
     <li><a href="home.php?page=london" class="<?php echo ($_GET['page'] ?? '') === 'london'  ? 'active' : ''; ?>">London</a></li>
       <li><a href="home.php?page=paris" class="<?php echo ($_GET['page'] ?? '') === 'paris'  ? 'active' : ''; ?>">Paris</a></li>
       <li><a href="home.php?page=tokyo" class="<?php echo ($_GET['page'] ?? '') === 'tokyo'  ? 'active' : ''; ?>">Tokyo</a></li>
+      <li><a href="home.php?page=myinfo" class="<?php echo ($_GET['page'] ?? '') === 'myinfo'  ? 'active' : ''; ?>">My Info</a></li>
     </ul>
   </nav>
   
@@ -103,7 +104,9 @@ footer {
                         case 'tokyo':
                             include 'tokyo.php';
                             break;
-            
+                            case 'myinfo':
+                              include 'myinfo.php';
+                              break;
         }
     }
      ?>
---
+++ myinfo.php
@@ -0,0 +1,4 @@
+<h1>Trixy Genon</h1>
+<p>PHINMA Student</p>
+<p>King Arthur, Puntod</p>
+<p>Female</p>
