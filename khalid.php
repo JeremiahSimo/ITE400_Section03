806a39e0be1170a981804633c25bdd5dc138e337
unknown <jare.khalid.coc@phinmaed.com>
2024-12-11 08:10:31 +0800

    info updated

---
+++ khalid.php
@@ -0,0 +1,5 @@
+
+<h1>Khalid, Jamil R.</h1>
+    <p>A student of Information Technology in Phinma Cagayan de Oro College</p>
+    <p>Is a digital artist</p>  
+    <p>Is currently 19 years of age</p>
