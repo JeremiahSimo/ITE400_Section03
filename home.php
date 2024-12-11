a73a74e36f068d8c748ae05f6004e79a3ba08685
unknown <rugayregine@gmail.com>
2024-12-11 08:52:15 +0800

    updated

--- home.php
+++ home.php
@@ -84,6 +84,7 @@ footer {
     <li><a href="home.php?page=london" class="<?php echo ($_GET['page'] ?? '') === 'london'  ? 'active' : ''; ?>">London</a></li>
       <li><a href="home.php?page=paris" class="<?php echo ($_GET['page'] ?? '') === 'paris'  ? 'active' : ''; ?>">Paris</a></li>
       <li><a href="home.php?page=tokyo" class="<?php echo ($_GET['page'] ?? '') === 'tokyo'  ? 'active' : ''; ?>">Tokyo</a></li>
+      <li><a href="home.php?page=branch" class="<?php echo ($_GET['page'] ?? '') === 'tokyo'  ? 'active' : ''; ?>">branch</a></li>
     </ul>
   </nav>
   
